ec85f338b4babb558f4619bcf8228119e7542fdd
Rik Prohaska <prohaska7@gmail.com>
2015-10-31 16:53:07 -0400

    test cpu affinity and number of cpus

--- portability/tests/test-active-cpus.cc
+++ portability/tests/test-active-cpus.cc
@@ -43,23 +43,72 @@ Copyright (c) 2006, 2015, Percona and/or its affiliates. All rights reserved.
 #include <toku_assert.h>
 #include "toku_os.h"
 
-int main(void) {
-    int r;
-    r = unsetenv("TOKU_NCPUS"); 
-    assert(r == 0);
+#if HAVE_SCHED_H
+#include <sched.h>
 
-    int max_cpus = sysconf(_SC_NPROCESSORS_ONLN);
-    assert(toku_os_get_number_cpus() == max_cpus);
-
-    // change the TOKU_NCPUS env variable and verify that the correct number is computed
-    for (int ncpus = 1; ncpus <= max_cpus; ncpus++) {
-        char ncpus_str[32];
-        sprintf(ncpus_str, "%d", ncpus);
-        r = setenv("TOKU_NCPUS", ncpus_str, 1);
-        assert(r == 0);
-
-        assert(toku_os_get_number_cpus() == ncpus);
+static void check_number_cpus(int ncpus, int cpus_online) {
+    // set random cpu's in the cpu set
+    cpu_set_t cs;
+    if (ncpus == cpus_online) {
+        CPU_ZERO(&cs);
+        for (int cpu = 0; cpu != ncpus; cpu++) {
+            CPU_SET(cpu, &cs);
+        }
+    } else {
+        CPU_ZERO(&cs);
+        for (int n = 0; n != ncpus; ) {
+            int cpu = rand() % cpus_online;
+            if (!CPU_ISSET(cpu, &cs)) {
+                CPU_SET(cpu, &cs);
+                n++;
+            }
+        }
     }
+    int r = sched_setaffinity(0, sizeof cs, &cs);
+    assert(r == 0);
+    assert(toku_os_get_number_cpus() == ncpus);
+}
+#endif
+
+int main(void) {
+    // test the default case of unset TOKU_NCPUS and no affinity
+    {
+        int r = unsetenv("TOKU_NCPUS"); 
+        assert(r == 0);
+        int cpus_online = sysconf(_SC_NPROCESSORS_ONLN);
+        assert(cpus_online > 0);
+        assert(toku_os_get_number_cpus() == cpus_online);
+    }
+
+    // verify that TOKU_NCPUS env var gets priority
+    {
+        int r = unsetenv("TOKU_NCPUS"); 
+        assert(r == 0);
+        int cpus_online = sysconf(_SC_NPROCESSORS_ONLN);
+        assert(cpus_online > 0);
+        for (int ncpus = 1; ncpus <= cpus_online; ncpus++) {
+            char ncpus_str[32];
+            sprintf(ncpus_str, "%d", ncpus);
+            r = setenv("TOKU_NCPUS", ncpus_str, 1);
+            assert(r == 0);
+            assert(toku_os_get_number_cpus() == ncpus);
+        }
+    }
+
+#if HAVE_SCHED_GETAFFINITY
+    // test various cpu affinities
+    {
+        int r = unsetenv("TOKU_NCPUS"); 
+        assert(r == 0);
+        int cpus_online = sysconf(_SC_NPROCESSORS_ONLN);
+        assert(cpus_online >= 0);
+        for (int ncpus = 1; ncpus <= cpus_online; ncpus++) {
+            for (int nexp = 0; nexp < 100; nexp++) {
+                check_number_cpus(ncpus, cpus_online);
+            }
+        }
+    }
+#endif
 
     return 0;
 }
